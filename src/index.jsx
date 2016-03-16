f217867d24a2483e82394087dba4e4cb8ab6dd05
Josh Stricklin <jostricklin@gmail.com>
2016-03-16 13:51:57 -0500

    create main App component, use react router to render root component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Hello from './components/Hello';
 
 console.log('It lives!');
 
+const routes = <Route component={App}>
+	<Route path="/" component={Hello} />
+</Route>;
+
 ReactDOM.render(
-	<Hello />,
+	<Router history={hashHistory}>{routes}</Router>,
 	document.getElementById('app')
 );
