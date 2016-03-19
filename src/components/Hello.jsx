4d17f67aa26baee2167f57edfd8a915a7f0f866b
Josh Stricklin <jostricklin@gmail.com>
2016-03-19 13:59:46 -0500

    call CounterContainer in root route

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {CounterContainer} from './Counter';
 
 export default React.createClass({
 	mixins: [PureRenderMixin],
 	render: function() {
 		return <div className="hello">
 			<h1>Hello, World!</h1>
+			<div>
+				<CounterContainer />
+			</div>
 		</div>
 	}
 })
