8739a5bebb1dcdabb957154fadb622c9475e96a5
Josh Stricklin <jostricklin@gmail.com>
2016-03-25 13:05:23 -0500

    install Rucksack

--- src/components/Another.jsx
+++ src/components/Another.jsx
@@ -5,7 +5,8 @@ export default React.createClass({
 	mixins: [PureRenderMixin],
 	render: function() {
 		return <div className="hello">
-			<h1>Hello from a new route and a new component!</h1>
+			<h1>Look at all the neat stuff we can do with PostCSS!</h1>
+			<h4>Some Lost grid columns:</h4>
 			<section>
 				<div className="first lost">first</div>
 				<div className="second lost">second</div>
